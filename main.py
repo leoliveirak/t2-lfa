78345af8c3fe84544f0876b8c0aabfce96f278c1
leoliveirak <klitzkel789@gmail.com>
2025-05-28 13:45:20 -0300

    correção bugs + ajustes de lógica

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
 from pda import AP
+from constants import EPSILON
 
 # Definição do autômato de pilha
 Q = ['q0', 'q1', 'q2', 'q3', 'q4']
-Sigma = ['T', 'C', 'A', 'G', 'EMPTY']
-gama = ['$', 'T', 'A', 'EMPTY']
+Sigma = ['T', 'C', 'A', 'G', EPSILON]
+gama = ['$', 'T', 'A', EPSILON]
 delta = {
-	('q0', 'EMPTY', 'EMPTY'): ('q1', '$'),
-	
+	('q0', EPSILON, EPSILON): ('q1', '$'),
+    
+	('q1', 'G', '$'): ('q1', '$'),
+	('q1', 'G', 'T'): ('q1', 'T'),
+	('q1', 'G', 'A'): ('q1', 'A'),
+    
 	('q1', 'C', '$'): ('q1', '$'),
-	('q1', 'C', 'T'): ('q1', 'EMPTY'),
-	('q1', 'C', 'A'): ('q1', 'EMPTY'),
-	('q1', 'T', 'A'): ('q1', 'EMPTY'),
-	('q1', 'A', 'T'): ('q1', 'EMPTY'),
+	('q1', 'C', 'T'): ('q1', 'T'),
+	('q1', 'C', 'A'): ('q1', 'A'),
+    
+	('q1', 'A', 'T'): ('q1', EPSILON),
 	('q1', 'A', '$'): ('q2', '$'),
 	('q1', 'A', 'A'): ('q2', 'A'),
+    
+	('q1', 'T', 'A'): ('q1', EPSILON),
 	('q1', 'T', '$'): ('q3', '$'),
-	('q1', 'T', 'T'): ('q3', 'EMPTY'),
+	('q1', 'T', 'T'): ('q3', 'T'),
 	
-	('q2', 'EMPTY', 'EMPTY'): ('q1', 'A'),
+	('q1', EPSILON, '$'): ('q4', EPSILON),
 	
-	('q3', 'EMPTY', 'EMPTY'): ('q1', 'T'),
+	('q2', EPSILON, EPSILON): ('q1', 'A'),
 	
-	('q1', 'EMPTY', '$'): ('q4', 'EMPTY'),
+	('q3', EPSILON, EPSILON): ('q1', 'T'),
 }
 F = ['q4']
 
 # Criação do autômato de pilha
 pda = AP(Q, Sigma, gama, delta, 'q0', F)
 
+# Testes
+# sequencia_testes = {
+    
+# }
+
 # Teste do autômato de pilha com uma entrada
-entrada = 'T C A T C A T C A T C A T C A T C A' 
+entrada = "AAACGCCTCATTAAAGTGGTTT" 
 
-entrada = entrada.strip().split()  # Divide a entrada em símbolos
+#entrada = entrada.strip().split()  # Divide a entrada em símbolos
 
-# Adiciona o símbolo 'EMPTY' ao final da entrada
-entrada.append('EMPTY') 
+# Adiciona o símbolo EPSILON ao final da entrada
+#entrada.append(EPSILON) 
 
-# Adiciona o símbolo 'EMPTY' ao início da entrada
-entrada.insert(0, 'EMPTY')
+# Adiciona o símbolo EPSILON ao início da entrada
+#entrada.insert(0, EPSILON)
 
 
 resultado = pda.run(entrada)
--- pda.py
+++ pda.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+from constants import EPSILON
 class AP:
     def __init__(self, Q, Sigma, gama, delta, q0, F):
         self._Q = Q
@@ -16,10 +16,10 @@ class AP:
         self._stack.clear()
 
     def realizar_transicao(self, q, appendpilha, poppilha):
-        if(poppilha != 'EMPTY'):
+        if(poppilha != EPSILON):
             self._stack.pop()
         
-        if(appendpilha != 'EMPTY'):
+        if(appendpilha != EPSILON):
             self._stack.append(appendpilha)
         
         if(q not in self._Q):
@@ -30,16 +30,23 @@ class AP:
     def run(self, entrada):
         self.clean()
 
-        self._stack.append('EMPTY')
+        self._stack.append(EPSILON)
 
         for simbolo in entrada:
-            print(f"DEBUG: qA: {self.qA}, w[i]: {simbolo}, Pilha: {list(self._stack)}")
+            #print(f"DEBUG: Delta completo em uso: {self._delta}")
+            print(f"DEBUG: Chave sendo buscada: {(self.qA, simbolo, self._stack[-1])}")
+            #print(f"DEBUG: qA: {self.qA}, w[i]: {simbolo}, Pilha: {list(self._stack)}")
             if(self.fazer_transicao_vazia_se_existir()):
-                continue
+                print(f"DEBUG: qA: {self.qA}, w[i]: {simbolo}, Pilha: {list(self._stack)}")
+                #continue
             transicao = self._delta.get((self.qA, simbolo, self._stack[-1]))
-            print(f"DEBUG: qA: {self.qA}, w[i]: {simbolo}, Pilha: {list(self._stack)}")
             if transicao:
+                print(f"DEBUG: qA: {self.qA}, w[i]: {simbolo}, Pilha: {list(self._stack)}")
                 self.realizar_transicao(transicao[0], transicao[1], self._stack[-1])
+        ts = self._delta.get((self.qA, EPSILON, self._stack[-1]));
+        if (ts):
+            #print(f"DEBUG: qA: {self.qA}, w[i]: {simbolo}, Pilha: {list(self._stack)}")
+            self.realizar_transicao(ts[0], ts[1], self._stack[-1])
 
         if self.qA in self._F:
             return True
@@ -47,10 +54,10 @@ class AP:
             return False
 
     def fazer_transicao_vazia_se_existir(self):
-        transicao = self._delta.get((self.qA, 'EMPTY', 'EMPTY'))
+        transicao = self._delta.get((self.qA, EPSILON, EPSILON))
         if transicao:
             print(f"DEBUG: Fez transição vazia: {transicao}")
-            self.realizar_transicao(transicao[0], transicao[1], 'EMPTY')
+            self.realizar_transicao(transicao[0], transicao[1], EPSILON)
             return True
         return False
         
