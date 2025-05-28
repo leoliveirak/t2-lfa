9df3686b74e35ec39199f2f555bed8175fc8e48a
leoliveirak <klitzkel789@gmail.com>
2025-05-27 23:16:50 -0300

    Classe AP: atributos

---
+++ pda.py
@@ -0,0 +1,8 @@
+class AP:
+    def __init__(self, Q, Sigma, gama, delta, q0, F):
+        self._Q = Q
+        self._Sigma = Sigma
+        self._gama = gama
+        self._delta = delta
+        self._q0 = q0
+        self._F = F
