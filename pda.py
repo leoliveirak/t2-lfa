571f6f5eb7e0e6a56ac9bb10cc8ab01d83c0c0c9
leoliveirak <klitzkel789@gmail.com>
2025-05-28 07:19:09 -0300

    realizar_transicao

--- pda.py
+++ pda.py
@@ -6,3 +6,18 @@ class AP:
         self._delta = delta
         self._q0 = q0
         self._F = F
+        self._estado_atual = q0
+
+    def realizar_transicao(self, q, appendpilha, poppilha, pilha):
+        if(poppilha != 'EMPTY'):
+            pilha.pop()
+        
+        if(appendpilha != 'EMPTY'):
+            pilha.append(appendpilha)
+        
+        if(q not in self._Q):
+            raise Exception(f"Estado {q} não pertence ao conjunto de estados Q")
+        else:
+            self._estado_atual = q
+        
+        return pilha
